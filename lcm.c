a7d610f543f19b87a2df2b99b0d450564a91df60
charlottebrink <cbrink14@vt.edu>
2018-02-08 14:37:44 -0500

    Add files via upload

--- lcm.c
+++ lcm.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+int gcd(int a, int b){
+	if(b==0){
+	return a;}
+	return gcd(b,a%b);
+}
 int main(){
 int num1=malloc(sizeof(int));
 int num2=malloc(sizeof(int));
@@ -7,14 +11,9 @@ printf("Enter the first number:");
 scanf("%i", num1);
 printf("Enter the second number:");
 scanf("%i", num2);
-int g=gcd(num1,num2);
-printf("The gcd between the two numbers input is %i",g);
+int g=(num1*num2)/gcd(num1,num2);
+printf("The lcm between the two numbers input is %i",g);
 free(num1);
 free(num2);
 return g;
-}
-int gcd(int a, int b){
-	if(b==0){
-	return a;}
-	return gcd(b,a%b);
 }
